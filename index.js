f4be8dba1c9fe8cd2a2247d66eaff9ebb4eb3f06
sleeplesseditor <josh@bryson.me.uk>
2019-02-16 17:00:00 +0000

    Mine block post request initial setup

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
 });
 
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+
+    blockchain.addBlock({ data });
+
+    res.redirect('/api/blocks');
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Listening at localhost:${PORT}`);
