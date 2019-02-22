2d218d5a640d27e1101734e6a08fe363a57854bd
sleeplesseditor <josh@bryson.me.uk>
2019-02-22 07:51:21 +0000

    BodyParser installed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
